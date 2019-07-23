f0c7eebb5c5bc60ebe882542c372a7251fc17297
Mark.Dochstader <mark.dochstader@aucerna.com>
2019-07-23 11:57:03 -0600

    02 Refactor to Loosen Coupling between Presentation and Data Layers
    1. Improve Abstraction (add Interfaces)
    2. Dictate the Service outside of the Client (use Constructor Injection)
    3. Focus on Object Composition (snap together pieces)

--- Pets.Viewer/App.xaml.cs
+++ Pets.Viewer/App.xaml.cs
@@ -5,13 +5,26 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Pets.DataAccess;
+using Pets.Presentation;
 
 namespace Pets.Viewer
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Compose();
+            Application.Current.MainWindow.Show();
+        }
+
+        private static void Compose()
+        {
+            var reader = new ServiceReader();
+            var viewModel = new PetsViewModel(reader);
+            Application.Current.MainWindow = new PetViewerWindow(viewModel);
+        }
     }
 }
