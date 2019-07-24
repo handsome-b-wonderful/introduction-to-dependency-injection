036a03c7871a51fa001dbca9be3cf5e141ae0176
Mark.Dochstader <mark.dochstader@aucerna.com>
2019-07-24 13:05:10 -0600

    03a Add Support for Csv Data
    Implement the Csv Data Reader

---
+++ Pets.DataAccess/CsvFileLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace Pets.DataAccess
+{
+    public class CsvFileLoader : ICsvFileLoader
+    {
+        private readonly string _filePath;
+
+        public CsvFileLoader(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public string LoadFile()
+        {
+            using (var reader = new StreamReader(_filePath))
+                return reader.ReadToEnd();
+        }
+    }
+}
